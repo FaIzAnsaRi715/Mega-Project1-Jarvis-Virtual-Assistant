19eefe8d746cbecb956ba32cf49c83d1130c7191
FaIzAnsaRi715 <167748258+FaIzAnsaRi715@users.noreply.github.com>
2025-07-24 18:23:11 +0530

    client.py

--- client.py
+++ client.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
     base_url="https://openrouter.ai/api/v1",
-    api_key="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"
+    api_key="Enter your API key"
 )
 completion = client.chat.completions.create(
     model = "deepseek/deepseek-chat-v3-0324:free",
